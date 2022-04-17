c73d5a1d47d67292cd632ec17d4d3032f4c5ffc7
Bhanu3270 <bhanu006808@gmail.com>
2022-04-17 19:22:59 +0530

    make file

---
+++ .vscode/avr.properties.mk
@@ -0,0 +1,4 @@
+AVR.source.compiler = C:\Users\Bhanu\M2_LED_MATRIX\M2_LED_MATRIX\3_implementation\main.c
+AVR.source.libraries = C:\Users\Bhanu\M2_LED_MATRIX\M2_LED_MATRIX\3_implementation
+AVR.device.type = 
+AVR.device.frequency = 

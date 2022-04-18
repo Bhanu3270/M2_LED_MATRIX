11fa9a24561d8409a6d39e0f32a09fc49bb07b14
Bhanu3270 <bhanu006808@gmail.com>
2022-04-18 11:04:35 +0530

    make file  cpp json file

--- .vscode/avr.properties.mk
+++ .vscode/avr.properties.mk
@@ -1,4 +1,4 @@
 AVR.source.compiler = C:\Users\Bhanu\M2_LED_MATRIX\M2_LED_MATRIX\3_implementation\main.c
-AVR.source.libraries = C:\Users\Bhanu\M2_LED_MATRIX\M2_LED_MATRIX\3_implementation
-AVR.device.type = 
-AVR.device.frequency = 
+AVR.source.libraries = C:\Users\Bhanu\M2_LED_MATRIX\M2_LED_MATRIX\3_implementation C:\WinAVR-20100110\avr32\include
+AVR.device.type = ATmega328
+AVR.device.frequency = 1000000
